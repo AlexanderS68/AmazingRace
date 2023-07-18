d415a4889069dd66d47e1364eabdc1065a359f16
AlexanderS68 <80540025+AlexanderS68@users.noreply.github.com>
2023-07-18 15:39:33 -0400

    add README

--- AmazingRaceSolution/Solution.cs
+++ AmazingRaceSolution/Solution.cs
@@ -1,27 +1,33 @@
-﻿namespace AmazingRaceSolution{
+﻿namespace AmazingRaceSolution
+{
     public class Solution
     {
-        static void Main() {
+        static void Main()
+        {
             var maze = ReadMaze("maze.txt");
 
-            var start = new Point(0, Array.IndexOf(maze[0], 1));
-            var end = new Point(maze.Length - 1, Array.LastIndexOf(maze[^1], 1));
+            var startPoint = new Point(0, Array.IndexOf(maze[0], 1));
+            var endPoint = new Point(maze.Length - 1, Array.LastIndexOf(maze[^1], 1));
 
-            // Find a path from the start to the end points
-            var path = FindPath(maze, start, end, new List<Point> { start });
-            if (path != null) {
-                foreach (var point in path) {
-                    Console.Write($"({point.X}, {point.Y}) ");
+            // Find a path from the start point to the end point
+            var path = FindPath(maze, startPoint, endPoint, new List<Point> { startPoint });
+            if (path != null)
+            {
+                foreach (var position in path)
+                {
+                    Console.Write($"({position.X}, {position.Y}) ");
                 }
-            } else {
+            }
+            else
+            {
                 Console.WriteLine("No valid path found in the maze.");
             }
         }
 
-        // Reads the maze from the file maze.txt, with each line converted to an array of integers
-        static int[][] ReadMaze(string path)
+        // Reads the maze from the file "maze.txt", with each line converted to an array of integers
+        static int[][] ReadMaze(string filePath)
         {
-            var lines = File.ReadAllLines(path);
+            var lines = File.ReadAllLines(filePath);
             var maze = new int[lines.Length][];
             for (int i = 0; i < lines.Length; i++)
             {
@@ -30,7 +36,7 @@
             return maze;
         }
 
-        // Recursive function to find a path from the start point to end point in the maze
+        // Recursive function to find a path from the start point to the end point in the maze
         public static List<Point>? FindPath(int[][] maze, Point current, Point end, List<Point> path)
         {
             if (current.Equals(end)) return path;
@@ -44,7 +50,7 @@
                     if (result != null) return result;
                 }
             }
-            return null; // no path found
+            return null;
         }
 
         // Function to get all valid neighbors of a point in a maze
@@ -56,10 +62,10 @@
                 new Point(0, -1), // left
                 new Point(0, 1)   // right
             };
-            foreach (var dir in directions)
+            foreach (var direction in directions)
             {
-                var x = point.X + dir.X;
-                var y = point.Y + dir.Y;
+                var x = point.X + direction.X;
+                var y = point.Y + direction.Y;
                 if (x >= 0 && x < maze.Length && y >= 0 && y < maze[0].Length && maze[x][y] == 1)
                 {
                     yield return new Point(x, y);
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# AmazingRace
+# Amazing Race Solution
+
+This program solves a maze by finding a path from the start point to the end point. It uses a recursive algorithm to explore all possible paths in the maze.
+
+## How to Run the Program
+
+1. Make sure you have the .NET Core SDK installed on your machine. You can download it from the official .NET website: [https://dotnet.microsoft.com/download](https://dotnet.microsoft.com/download)
+
+2. Clone or download the project source code to your local machine.
+
+3. Open a terminal or command prompt and navigate to the directory containing the project files.
+
+4. Run the following command to build the project:
+
+   ```shell
+   dotnet build
+   ```
+
+5. Prepare your maze by creating a text file (e.g., `maze.txt`) with the following format:
+   - Each line represents a row in the maze.
+   - Use spaces or commas to separate the values in each row.
+   - Use `0` to represent empty cells or walls, and `1` to represent valid path cells.
+
+6. Run the program using the following command:
+
+   ```shell
+   dotnet run 
+   ```
+
+   This will execute the `Main` method of the `Solution` class, which reads the maze from `maze.txt`, finds a path from the start point to the end point, and displays the path coordinates.
+
+7. The program will output the coordinates of the path in the maze. If a valid path is found, it will be displayed as a series of `(x, y)` points. If no valid path is found, it will show a message indicating that no path was found in the maze.
+
+## Additional Notes
+
+- The `Solution` class is part of the `AmazingRaceSolution` namespace. It contains the logic for reading the maze, finding the path, and executing the program.
+- The `Point` class is defined in the same namespace and represents a point in a two-dimensional space.
+- The `FindPath` method uses a recursive algorithm to explore the maze and find a valid path from the start point to the end point.
+- The `GetNeighbors` method retrieves all valid neighboring points of a given point in the maze.
+- The program assumes that the maze is rectangular (each row has the same length), and the maze file is correctly formatted.
+
+Feel free to customize the maze by modifying the `maze.txt` file or adapting the code to suit your specific needs. Enjoy solving the maze!
